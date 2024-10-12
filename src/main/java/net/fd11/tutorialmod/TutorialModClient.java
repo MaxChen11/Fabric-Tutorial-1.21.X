de4875ebcd560e26b3d17548fdf8c2f688a519e8
Max Sone <pyaesone.max@gmail.com>
2024-10-11 21:46:55 -0600

    Initial commit

---
+++ src/main/java/net/fd11/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.fd11.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
